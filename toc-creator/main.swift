ba577e315b3dbd748755360c3d40b0257cecbcdf
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-30 21:20:17 -0400

    Create a nested TOC outline

--- toc-creator/main.swift
+++ toc-creator/main.swift
@@ -15,15 +15,55 @@ let document = PDFDocument(url: pdfUrl)!
 let outlineRoot = document.outlineRoot
 print("Number of children", outlineRoot?.numberOfChildren ?? "No children")
 
-let newOutlineRoot = PDFOutline()
-newOutlineRoot.label = "Root Label"
+struct TocItem {
+    var label: String = ""
+    var pageNum: Int = 0
+    var children: [TocItem] = []
+}
 
-let firstChild = PDFOutline()
-firstChild.label = "First Child"
-let page = document.page(at: 100)!
-firstChild.destination = PDFDestination(page: page, at: CGPoint(x: 0.0, y: 0.0))
- 
-newOutlineRoot.insertChild(firstChild, at: 0)
+let outline: TocItem = TocItem(children: [
+    TocItem(
+        label: "Cover",
+        pageNum: 1
+    ),
+    TocItem(
+        label: "Contents",
+        pageNum: 5
+    ),
+    TocItem(
+        label: "Introduction",
+        pageNum: 6
+    ),
+    TocItem(
+        label: "Test 1",
+        pageNum: 12,
+        children: [
+            TocItem(
+                label: "Listening",
+                pageNum: 12,
+            ),
+            TocItem(
+                label: "Reading",
+                pageNum: 18,
+            ),
+        ],
+    ),
+])
+
+func toPdfOutline(tocItem: TocItem, document: PDFDocument) -> PDFOutline {
+    let pdfOutline = PDFOutline()
+    pdfOutline.label = tocItem.label
+    let pageNum = (tocItem.pageNum == 0) ? 0 : tocItem.pageNum - 1
+    pdfOutline.destination = PDFDestination(page: document.page(at: pageNum)!, at: CGPoint(x: 0.0, y: 0.0))
+    
+    for (index, child) in tocItem.children.enumerated() {
+        let childOutline = toPdfOutline(tocItem: child, document: document)
+        pdfOutline.insertChild(childOutline, at: index)
+    }
+    return pdfOutline
+}
+
+let newOutlineRoot = toPdfOutline(tocItem: outline, document: document)
 
 document.outlineRoot = newOutlineRoot
 
